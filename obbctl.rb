8b9da2d0c4385090700597de7b51ee1cd547c061
Aleem Isiaka <30846935+limistah@users.noreply.github.com>
2024-05-03 20:34:41 +0100

    Update obbctl.rb

--- obbctl.rb
+++ obbctl.rb
@@ -13,7 +13,7 @@ class Obbctl < Formula
       sha256 "0a00e95a358c698a8dd24711e0f1a51805c899e44a1f40d7157a634ad967101f"
 
       def install
-        bin.install "obbctl"
+        bin.install "ctl"
       end
     end
     if Hardware::CPU.intel?
@@ -21,7 +21,7 @@ class Obbctl < Formula
       sha256 "de3b3a4d4f23d62826fa128bad08d1ff0e43f1eb7868d8c96d9ec92e0be5f373"
 
       def install
-        bin.install "obbctl"
+        bin.install "ctl"
       end
     end
   end
@@ -32,7 +32,7 @@ class Obbctl < Formula
       sha256 "fca16fac1d3f89e4c9f9816c3e0e1c3633cd1622264c5727e0fdecb0bfd84092"
 
       def install
-        bin.install "obbctl"
+        bin.install "ctl"
       end
     end
     if Hardware::CPU.intel?
@@ -40,7 +40,7 @@ class Obbctl < Formula
       sha256 "b38dbc3dc576e33e4eca278d2826c13f63e42538ce44db7673300d9d7796bd06"
 
       def install
-        bin.install "obbctl"
+        bin.install "ctl"
       end
     end
   end
